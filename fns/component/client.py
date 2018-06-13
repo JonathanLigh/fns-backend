3159612bc6adfb8425695a681d6b4e11c2fbcfce
Ethan Zhou <ethan.zhou@emory.edu>
2018-06-13 16:21:12 -0400

    articles from sources

--- fns/component/client.py
+++ fns/component/client.py
@@ -1,5 +1,7 @@
+from typing import List
 from newsapi import NewsApiClient
 
+from constant.strconst import StringConstants
 from util.resutils import *
 
 
@@ -18,5 +20,23 @@ class FNSClient:
 
         return all_sources
 
-    def get_articles_by_source(self, source):
-        pass
+    def get_articles_by_sources(self, sources: List[str]):
+        csv_sources = StringConstants.COMMA.join(sources)
+
+        articles_by_sources = []
+
+        page_num = 1
+        initial_results = self.news_client.get_everything(sources=csv_sources, page=page_num, page_size=100)
+        total_num_articles = initial_results["totalResults"]
+        page_one_articles = initial_results["articles"]
+
+        articles_by_sources.extend(page_one_articles)
+        remaining_articles = total_num_articles - len(articles_by_sources)
+
+        while remaining_articles > 0:
+            page_num += 1
+            page_articles = self.news_client.get_everything(sources=csv_sources, page=page_num, page_size=100)["articles"]
+            articles_by_sources.extend(page_articles)
+            remaining_articles = total_num_articles - len(articles_by_sources)
+
+        return articles_by_sources
