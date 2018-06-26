2c220893322d215f770d7a1fecd610dbd3583a23
Jonathan Ligh <jonathankligh@gmail.com>
2018-06-26 16:42:35 -0400

    example data accessible through base route

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json, Response
 
 from fns.db import init_db, db_session
 from colorama import Fore, init
@@ -12,10 +12,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost:5432/fns"
 
 @app.route('/')
 def hello_world():
-    return [{'title': 'The Big Gay', 'content': 'This past weekend marked the 25th annual...','date': '2018-06-26', 'catagory': 'Business'},
+    response = Response(
+        response = json.dumps([{'title': 'The Big Gay', 'content': 'This past weekend marked the 25th annual...','date': '2018-06-26', 'catagory': 'Business'},
             {'title': 'Chaos at the 50th Annual Impact Summit', 'content': 'Chaos errupts from the 50th annual Impact summit as generations of dreamers wake up.', 'date': '2018-06-26', 'catagory': 'Tech'},
-            {'title': 'Pangea Separates After What Also Felt Like 270 Million Years of Nothing', 'content': 'Founder and CEO, who cares, of the now un-trademarked Pangea today announced, to no one\'s surprise, the LLC\'s dissolution.', 'date': '2018-06-26', 'catagory': 'Comedy'}]
-
+            {'title': 'Pangea Separates After What Also Felt Like 270 Million Years of Nothing', 'content': 'Founder and CEO, who cares, of the now un-trademarked Pangea today announced, to no one\'s surprise, the LLC\'s dissolution.', 'date': '2018-06-26', 'catagory': 'Comedy'}]),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.teardown_appcontext
 def shutdown_session(ecpection=None):
