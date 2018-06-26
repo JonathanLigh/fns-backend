d0de03e69786447caac3861e44fb5b2530dfacc7
Jonathan Ligh <jonathankligh@gmail.com>
2018-06-26 16:00:54 -0400

    added test content to be sent to the front end

--- server.py
+++ server.py
@@ -12,7 +12,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost:5432/fns"
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return [{'title': 'The Big Gay', 'content': 'This past weekend marked the 25th annual...','date': '2018-06-26', 'catagory': 'Business'},
+            {'title': 'Chaos at the 50th Annual Impact Summit', 'content': 'Chaos errupts from the 50th annual Impact summit as generations of dreamers wake up.', 'date': '2018-06-26', 'catagory': 'Tech'},
+            {'title': 'Pangea Separates After What Also Felt Like 270 Million Years of Nothing', 'content': 'Founder and CEO, who cares, of the now un-trademarked Pangea today announced, to no one\'s surprise, the LLC\'s dissolution.', 'date': '2018-06-26', 'catagory': 'Comedy'}]
 
 @app.teardown_appcontext
 def shutdown_session(ecpection=None):
