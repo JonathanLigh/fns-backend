1616d6a4ed613e7bf91368f4dabf050b0fcb7767
Ethan Zhou <ethan.zhou@emory.edu>
2018-06-28 22:33:56 -0400

    db pathing bugfix

--- fns/db/__init__.py
+++ fns/db/__init__.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from fns.db.session import create_db_session
 from fns.util.resutils import get_database_paths
 
-database_paths = os.environ.get("DATABASE_URL") if os.environ.get("DATABASE_URL") else get_database_paths()
-engine, db_session = create_db_session(database_paths["LOCAL"])
+database_path = os.environ.get("DATABASE_URL") if os.environ.get("DATABASE_URL") else get_database_paths()["LOCAL"]
+engine, db_session = create_db_session(database_path)
 
 Base = declarative_base()
 Base.query = db_session.query_property()
